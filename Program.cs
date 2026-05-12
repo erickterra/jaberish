90f0e86ced90d7a289d454d9d0151a6b33e01744
Erick G Terra <erick@gflow.com.br>
2026-05-12 10:29:43 -0300

    Configuração Render

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@ using Jaberish.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+builder.WebHost.UseUrls($"http://*:{port}");
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
